700504eadfae1621f91958b2d0c9b1f37bb589bb
Sherstneva Tatyana <nikolina.tatyana@yandex.ru>
2023-11-27 17:37:51 +0500

    адаптив слайдеров

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 'auto',
     direction: 'horizontal',
     navigation: {
       nextEl: '.swiper-button-next',
@@ -9,10 +8,27 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      376: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      769: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      1441: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
   });
 
   const gallery = new Swiper('.gallery', {
-    slidesPerView: 'auto',
+   // slidesPerView: '4',
     direction: 'horizontal',
     navigation: {
       nextEl: '.button-next',
@@ -21,5 +37,25 @@ const swiper = new Swiper('.swiper', {
  
     scrollbar: {
       el: '.slider__progressbar',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      376: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+        grid: {
+          rows: 2,
+        },
+      },
+      769: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      1441: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
     },
   });
